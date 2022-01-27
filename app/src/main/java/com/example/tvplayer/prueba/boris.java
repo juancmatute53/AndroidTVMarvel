af57ece4d46d90fcf6815891cce3e5b7c38dc90d
Boris <hermosokawai@gmail.com>
2022-01-27 09:38:40 -0500

    dde boirs

---
+++ app/src/main/java/com/example/tvplayer/prueba/boris.java
@@ -0,0 +1,17 @@
+package com.example.tvplayer.prueba;
+
+public class boris {
+    private String id;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public boris(String id) {
+        this.id = id;
+    }
+}
